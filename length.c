402bfeec03ad689089ccc2e3254041fb143bba10
dseekatz <dseekatz@gmail.com>
2016-11-28 10:50:28 -0500

    working length function

---
+++ length.c
@@ -0,0 +1,42 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/* This file defines the node structure for a doubly linked list
+ * as well as four functions to manipulate doubly linked lists
+ */
+
+/* Union definition for data to be stored in node structure */
+union Data {
+	int d;
+	int* ptrd;
+	float f;
+	float* ptrf;
+	char c;
+	char* ptrc;
+}; /* End union */
+
+/* Linked list node structure */
+struct Node {
+	struct Node* prev;
+	union Data data;
+	struct Node* next;
+}; /* End structure */
+
+/* We don't want to have to type "struct" all the time */
+typedef struct Node Node;
+
+/* Head and last node global variable definitions.  Head node ALWAYS contains an int
+ * describing the length of the list.
+ */
+/* Please move these definitions to main.c before assignment submission */
+Node head = {NULL,{2},NULL};
+Node last = {NULL,{0},NULL};
+
+int length() {
+	return head.data.d;
+} // end length
+
+/* The main function should be moved to main.c before assignment submission */
+int main() {
+	printf("%d\n",length());
+} /* End main */
