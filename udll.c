c3177a74b5def801f1be3461dc92cf285da5a776
dseekatz <dseekatz@gmail.com>
2016-11-29 13:23:48 -0500

    Update udll.c

--- udll.c
+++ udll.c
@@ -1,29 +1,32 @@
 #include<stdlib.h>
+#include "udll.h"
 
-/* This file defines the node structure for a doubly linked list
- * as well as four functions to manipulate doubly linked lists
+/* This file gives the concrete function definitions for the four required functions */
+
+/* This function returns the length of the linked list (note that the length is stored
+ * in the head node)
  */
+int length() {
+	return head.data.d;
+} // end length
 
-union Data {
-	int d;
-	int* ptrd;
-	float f;
-	float* ptrf;
-	char c;
-	char* ptrc;
-}; /* End union */
-
-/* Linked list node structure */
-struct Node {
-	struct Node* prev;
-	union Data data;
-	struct Node* next;
-}; /* End structure */
-
-/* We don't want to have to type "struct" all the time */
-typedef struct Node Node;
-
-/* The main function should be moved to main.c before assignment submission */
-int main() {
-
-} /* End main */
+/* This function returns the union containing the data stored in the Node at the specified
+ * index.
+ */
+union Data get(int index) {
+	Node* ptr = NULL;
+	int i;
+	if (index <= length() / 2) {
+		ptr = &head;
+		for (i = 0; i < index; i++) {
+			ptr=(*ptr).next;
+		} // end fot loop
+		return (*ptr).data;
+	} else {
+		ptr = &last;
+		for (i = 0; i < index; i++) {
+			ptr=(*ptr).prev;
+		} // end for loop
+		return (*ptr).data;
+	} // end conditional
+} // end get
