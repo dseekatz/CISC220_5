2014e0c645a13b0a1bead3db1f6ea46838952bd2
Brooke <13brr2@queensu.ca>
2016-11-30 16:11:06 -0500

    Update voidInsert.c

--- voidInsert.c
+++ voidInsert.c
@@ -5,18 +5,67 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-//Create a new node
+//Create a new node 
+//doubly linked list
+union Data {
+	int d;
+	int ptrd;
+	float f;
+	float* ptrf;
+	char c;
+	char* ptrc;
+} //end union
+
 typedef struct Node {
-	int data;
+	struct Node* ptr;
+	struct Node* ptr2;
+	struct Node* newNode;
+	union Data data;
 	struct Node* next;
-} Node;
+}
 
 struct Node *head; //Assign head as start of linked list
 
 void insert(int index, int data) { //union Data data) {
-	Node* curr;
-	curr = 0;
-	Node* newNode; 
+	Node* ptr = new Node();
+	Node* newNode;
+	newNode.data;
+	
+	//find length
+	length();
+	int length = length/2;
+	int i = 0;
+	ptr = &head;
+	Node* ptr2 = ptr.next;
+	
+	//The index is less than length/2
+	if (index < length/2) {
+		while (i != index-1) {
+			ptr = ptr->next;
+			i++
+		}
+		else {
+			(*ptr).ext = &newNode;
+			(*newNode).next = ptr2;
+			(*newNode).prev = ptr;
+			(*ptr2).prev = newNode;
+		}
+	}
+	//The index is greater than length/2
+	if (index > length/2) {
+		while (i < index) {
+			ptr = ptr->next;
+			i++
+		}
+		else {
+			ptr.next = &newNode;
+			newNode.next = &ptr2;
+			newNode.prev = &ptr;
+			ptr2.prev = &newNode;
+		}
+	}
+}
+
 
 //initialize current pointer to head
 int i = 0; //start at first element in linked list and traverse
